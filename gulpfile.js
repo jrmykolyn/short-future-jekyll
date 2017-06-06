2dbedd3831a07a099d2472b417e45c97a44ff239
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-06-05 23:45:59 -0400

    chore(gulp) - Roughed out 'gulpfile.js'. Added 'default', 'styles', and 'sass' tasks.

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+// --------------------------------------------------
+// IMPORT MODULES
+// --------------------------------------------------
+// Node
+
+// Vendor
+var gulp = require( 'gulp' );
+var sass = require( 'gulp-sass' );
+var PathMap = require( 'sfco-path-map' );
+
+// --------------------------------------------------
+// DECLARE VARS
+// --------------------------------------------------
+var PATHS = new PathMap( {
+	cssSrc: './css',
+	sassSrc: './_sass/styles.scss'
+} );
+
+console.log( PATHS ); /// TEMP
+
+// --------------------------------------------------
+// DEFINE TASKS
+// --------------------------------------------------
+gulp.task( 'default', [ 'styles' ] );
+
+gulp.task( 'styles', [ 'sass' ] );
+
+gulp.task( 'sass', function() {
+	gulp.src( PATHS.sassSrc )
+		.pipe(
+			sass( {
+				outputStyle: 'expanded'
+			} )
+		)
+		.pipe( gulp.dest( PATHS.cssSrc ) );
+} );
