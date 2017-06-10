ad7aa0a921e643b2c147e75c65ea4af4c3615e1c
Jesse Mykolyn <jrmykolyn@gmail.com>
2017-06-10 01:14:49 -0400

    Tweaked 'stringScrambler' logic/timing in JS files.

--- js/main.js
+++ js/main.js
@@ -11,14 +11,14 @@ var stringScrambler = require( 'sfco-string-scrambler' );
 	var titleElem = document.getElementById( 'title' );
 	var charElems = titleElem.querySelectorAll( 'span' );
 	var timeoutRef = null;
-	var timeoutDur = 10000 // 10s
+	var timeoutDur = 20000 // 10s
 	var intervalRef = null;
-	var intervalDur = 2000; // 2s
+	var intervalDur = 5000; // 2s
 
 	timeoutRef = setTimeout( function() {
 		intervalRef = setInterval( function() {
 			var el = Array.prototype.slice.call( charElems, 0 )[ Math.floor( Math.random() * charElems.length ) ];
-			el.innerHTML = stringScrambler( el.innerHTML );
+			el.innerHTML = stringScrambler( 'a' ); // NOTE - Invocation at left will scrambler 1x char.
 		}, intervalDur );
 	}, timeoutDur );
 } )();
